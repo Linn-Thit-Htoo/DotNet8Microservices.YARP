fbadbfcb2284f0ca8c17c39ab989b11f1207e6d3
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-17 02:19:55 +0630

    -

--- DotNet8Microservices.YARP.BlogMicroservice/Features/BlogController.cs
+++ DotNet8Microservices.YARP.BlogMicroservice/Features/BlogController.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet8Microservices.YARP.BlogMicroservice.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8Microservices.YARP.BlogMicroservice.Features
+namespace DotNet8Microservices.YARP.BlogMicroservice.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly DA_Blog _dA_Blog;
+
+    public BlogController(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
+        _dA_Blog = dA_Blog;
+    }
 
-        public BlogController(DA_Blog dA_Blog)
-        {
-            _dA_Blog = dA_Blog;
-        }
+    [HttpGet]
+    public IActionResult GetBlogs()
+    {
+        return Ok(_dA_Blog.GetBlogs());
+    }
 
-        [HttpGet]
-        public IActionResult GetBlogs()
-        {
-            return Ok(_dA_Blog.GetBlogs());
-        }
-
-        [HttpPost]
-        public IActionResult CreateBlog([FromBody] BlogModel blog)
-        {
-            return Ok(blog);
-        }
+    [HttpPost]
+    public IActionResult CreateBlog([FromBody] BlogModel blog)
+    {
+        return Ok(blog);
     }
 }
