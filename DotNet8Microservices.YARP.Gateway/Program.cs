8e399a6c106191496b2bce5897462f197b080fd9
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-18 13:25:44 +0630

    -

--- DotNet8Microservices.YARP.Gateway/Program.cs
+++ DotNet8Microservices.YARP.Gateway/Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,7 +22,6 @@ builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSecti
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
